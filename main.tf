eb17dcddc1020f9f61f7f5112851cb8ca5ff31d7
Aslin Dhurai C <aslin.dhurai@capestart.com>
2025-04-09 13:17:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,78 @@ provider "aws" {
   region = "us-west-2"   # Choose your AWS region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "myawsbucket4016my"  # Bucket name must be globally unique
+# 1. Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MyNewVPC"
+  }
 }
-resource "aws_subnet" "example" {
-  vpc_id            = "vpc-00b46f253689c3c7d"
+
+# 2. Create Subnet inside VPC
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
 
   tags = {
-    Name = "ExampleSubnet"
+    Name = "MySubnet"
+  }
+}
+
+# 3. Create Internet Gateway (for internet access)
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+# 4. Create Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+# 5. Associate Subnet with Route Table
+resource "aws_route_table_association" "my_rta" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# 6. Create Security Group (allow SSH)
+resource "aws_security_group" "my_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "AllowSSH"
   }
 }
 
