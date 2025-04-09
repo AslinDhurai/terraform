ec6a5a3b938e38a1171a7185ae37d6966242c0ce
Aslin Dhurai C <aslin.dhurai@capestart.com>
2025-04-09 12:56:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "myawsbucket4016my"  # Bucket name must be globally unique
 }
 
+resource "aws_instance" "ubuntu_instance" {
+  ami           = "ami-0fc5d935ebf8bc3bc"   # Ubuntu Server 22.04 LTS AMI ID for us-west-2 (Oregon)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyUbuntuInstance"
+  }
+}
