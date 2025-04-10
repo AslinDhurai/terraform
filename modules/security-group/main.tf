6031a96d7a3af126843d75f5d630377bc4aad76d
Aslin Dhurai C <aslin.dhurai@capestart.com>
2025-04-10 18:43:47 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -12,12 +12,26 @@ resource "aws_security_group" "this" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+      description = "HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+      description = "App Port 9090"
+      from_port   = 9090
+      to_port     = 9090
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]  # Allow from everywhere; for production, limit this.
+  }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
